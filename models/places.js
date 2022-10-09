0c500f1eb591c3515164a0f1c4bbb6c25338cea4
Leonel Alanis <l.alanis1989@gmail.com>
2022-10-08 23:10:06 -0500

    added index.js to models

--- models/places.js
+++ models/places.js
@@ -1,4 +1,17 @@
-//places array with made up data
+const mongoose = require('mongoose')
+module.exports.Place = require('./places')
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true},
+  pic: String,
+  cuisines: { type: String, required: true},
+  city: { type: String, default: 'Anytown'},
+  state: { type: String, default: 'USA'},
+  founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
+
+/*places array with made up data
 module.exports = [{
         name: 'H-Thai-ML',
         city: 'Seattle',
@@ -11,4 +24,5 @@ module.exports = [{
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
         pic: '/images/coding-cafe.jpg'
-      }]
+      }]*/
+
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -80,6 +80,10 @@ function new_form() {
                         <label htmlFor="cuisines">Cuisines</label>
                         <input className="form-control" id="cuisines" name="cuisines" required/>
                     </div>
+                    <div className="form-group">
+                        <label htmlFor="founded">Founded Year</label>
+                        <input className="form-control" id="founded" name="founded" />
+                    </div>
                     {/*submit button*/}
                     <input className="btn btn-primary" type="submit" value="Add Place"/>
                 </form>
