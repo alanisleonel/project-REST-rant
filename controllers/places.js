f4e309766bc5c02413b53f9bec78a2e3daecaf89
Leonel Alanis <l.alanis1989@gmail.com>
2022-10-11 23:13:14 -0500

    added an error message

--- controllers/places.js
+++ controllers/places.js
@@ -13,6 +13,7 @@ router.get('/', (req, res) => {
      res.render('error404')
    }) 
 })
+
 // POST
 router.post('/', (req, res) => {
   db.Place.create(req.body)
@@ -20,10 +21,22 @@ router.post('/', (req, res) => {
     res.redirect('/places')
   })
   .catch(err => {
-    console.log('err', err)
+    if (err && err.name == 'ValidationError') {
+      let message = 'Validation Error: '
+      for (var field in err.errors) {
+        message += `${field} was ${err.errors[field].value}. `
+        message += `${err.errors[field].message}`
+      }
+      console.log('Validation error message', message)
+      res.render('places/new', {message})
+    }
+    else {
     res.render('error404')
+    }
   })
 })
+
+
 // New
 router.get('/new', (req, res) => {
   res.render('places/new')
