57569111eda65258df69c1f39e9dbe58a6b07090
Leonel Alanis <l.alanis1989@gmail.com>
2022-10-12 22:46:09 -0500

    Comments load successfully

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const db = require('../models')
+const { populate } = require('../models/comment')
 
 
 // Index
@@ -46,7 +47,9 @@ router.get('/new', (req, res) => {
 // Show
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
+  .populate('comments')
   .then(place => {
+    console.log(place.comments)
     res.render('places/show', {place})
   })
   .catch(err => {
